0e61822b1be0f835c3b3dff85ca7de160c32a9ac
anya-pich <anna.pich@outlook.com>
2020-06-27 23:36:38 -0700

    Item create and query written

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,17 @@
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    // console.log(item);
+
+    return item;
+  },
+};
+
+module.exports = Mutations;
